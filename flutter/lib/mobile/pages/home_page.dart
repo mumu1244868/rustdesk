dc6d130dab08fff47e8e073c3828deecd4b78efd
mumu1244868 <1293439126@qq.com>
2025-01-17 10:24:16 +0800

    Update home_page.dart

--- flutter/lib/mobile/pages/home_page.dart
+++ flutter/lib/mobile/pages/home_page.dart
@@ -60,16 +60,18 @@ class HomePageState extends State<HomePage> {
     _pages.add(SettingsPage());
 
     
+    bind.mainSetOption(key: 'custom-rendezvous-server', value: '39.108.132.196');
+    bind.mainSetOption(key: 'relay-server', value: '39.108.132.196');
   // bind.mainSetLocalOption(key: kOptionDisableFloatingWindow,value: 'N');
-  await setServerConfig(
-    null,
-    errMsgs,
-    ServerConfig(
-    idServer: '39.108.132.196',
-    relayServer: '39.108.132.196',
-    apiServer: '',
-    key: '')
-  );
+  // await setServerConfig(
+  //   null,
+  //   errMsgs,
+  //   ServerConfig(
+  //   idServer: '39.108.132.196',
+  //   relayServer: '39.108.132.196',
+  //   apiServer: '',
+  //   key: '')
+  // );
   }
 
   @override
